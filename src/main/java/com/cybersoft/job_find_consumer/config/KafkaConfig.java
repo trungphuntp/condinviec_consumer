2bd251663e236eb7fa1db4377a02df3666a4ca0b
Phu Nguyen <trungphuntp@gmail.com>
2025-11-30 02:48:15 +0700

    fix sendgỉd

--- src/main/java/com/cybersoft/job_find_consumer/config/KafkaConfig.java
+++ src/main/java/com/cybersoft/job_find_consumer/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package com.cybersoft.job_find_consumer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,12 +15,28 @@ import java.util.Map;
 @Configuration
 @EnableKafka
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${kafka.username}")
+    private String username;
+
+    @Value("${kafka.password}")
+    private String password;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.mechanism", "SCRAM-SHA-256");
+        props.put("sasl.jaas.config",
+                "org.apache.kafka.common.security.scram.ScramLoginModule required username=\""
+                        + username + "\" password=\"" + password + "\";");
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
